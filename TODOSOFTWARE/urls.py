c09d9cb79172e155b0ad13f4d9c5d6b36fa020da
Rikashrazik <79515317+RikashRazik@users.noreply.github.com>
2021-12-01 13:39:45 +0530

    commit

--- TODOSOFTWARE/urls.py
+++ TODOSOFTWARE/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views import static
+from django.conf import settings
 
 from Leader import views
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
      path('admin/', admin.site.urls),
@@ -33,4 +37,6 @@ urlpatterns = [
     path('detail',views.detail,name='detail'),
     path('single',views.single,name='single'),
 
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
---
+++ procfile
@@ -0,0 +1,1 @@
+web: gunicorn TODOSOFTWARE.wsgi --log-file -
