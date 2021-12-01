621c373698b7d9475b95f376697ef72cb981cb57
Rikashrazik <79515317+RikashRazik@users.noreply.github.com>
2021-12-01 14:44:50 +0530

    commit

--- TODOSOFTWARE/settings.py
+++ TODOSOFTWARE/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--pl^j7xzns2xf%fbrmvl5agvf2hm5-*-j=#ofjutvlc49bu9_7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','todosoftware.herokuapp.com']
 
 
 # Application definition
